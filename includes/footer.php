bd6061ae1295e15f5419d9b404a01e5c1e9abdad
Syed Usama <syedusama875@gmail.com>
2023-06-08 05:50:47 -0700

    update

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,37 @@
-<footer>
-   <div class="container">
+<footer style="background-image: url(assets/images/bg-footer.png);">
+<div class="col-md-12 footer-img">
+	<img src="assets/images/footer-logo.png" alt="">
+</div>
+   <div class="container" >
    	<div class="row">
    		<div class="col-md-4">
    			<div class="widget">
-   				
+   				<h2>Contact Us</h2>
+				<h3>London Oxford Street, 012 United Kingdom.<br> userthemes@gmail.com <br>+032 3456 7890</h3>
    			</div>
    		</div>
    		<div class="col-md-4">
    			<div class="widget">
-   				
+			   <h2>About Us</h2>
+			   <h3>Share any space from a Shared Living room to a second home and everthing in between</h3>
+			   <i class="fab fa-facebook-f"></i>
+			   <i class="fab fa-twitter"></i>
+			   <i class="fab fa-instagram"></i>
+			   <i class="fab fa-google-plus-g"></i>
    			</div>
    		</div>
    		<div class="col-md-4">
    			<div class="widget">
-   				
+			   <h2>Gallery images</h2>
    			</div>
    		</div>
    	</div>
      
    </div>
+   <div class="copyright text-center">
+	   <div class="container">
+		   <p>All Rights Reserved</p>
+		   </div>
+	</div>
 </footer>
 
- <div class="copyright text-center">
- 	<div class="container">
-         <p>All Rights Reserved</p>
-         </div>
-  </div>
